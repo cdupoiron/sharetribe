42307991ade6d05786c6ad8490846450c2f35b77
Boyan Tabakov <boyan@sharetribe.com>
2016-11-14 16:28:57 +0200

    fixup: asset variables

--- script/prepare-assets.sh
+++ script/prepare-assets.sh
@@ -9,12 +9,8 @@ else
     # Set dummy database connection string
     export DATABASE_URL="mysql2://user:pass@127.0.0.1/dummy"
 
-    # Set these to correct values
-    # FIXME OS defaults
-    export font_proximanovasoft_url=https://assets-sharetribecom.sharetribe.com/webfonts/proximanovasoft/
-    export ss_pika_location=//assets-sharetribecom.sharetribe.com/webfonts/ss-pika/ss-pika.css
-    export ss_social_location=//assets-sharetribecom.sharetribe.com/webfonts/ss-pika/ss-social.css
-    export icon_pack=ss-pika
+    # Edit the script/asset-variables.sh file to e.g. set font locations, icon pack, etc.
+    [ -f "script/asset-variables.sh" ] && source "script/asset-variables.sh"
 
     secret_key_base=$(ruby -r securerandom -e "puts SecureRandom.hex(64)")
     export secret_key_base
